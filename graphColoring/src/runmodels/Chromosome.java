6ad2a87836ef9c55ff181d619b46250a09139926
Janette Marie Rounds <janette.rounds@msu.montana.edu>
2016-09-21 21:36:46 -0600

    Genetic Algorithm skeleton, more to follow

---
+++ graphColoring/src/runmodels/Chromosome.java
@@ -0,0 +1,10 @@
+package runmodels;
+
+import java.util.ArrayList;
+
+public class Chromosome {
+	protected int numVertices;
+	public ArrayList<Vertex> genes;
+	public int fitness;
+	
+}
--- graphColoring/src/runmodels/LocalSearchGA.java
+++ graphColoring/src/runmodels/LocalSearchGA.java
@@ -3,10 +3,34 @@ package runmodels;
 import java.util.ArrayList;
 
 public class LocalSearchGA extends AbstractAlgorithm {
+	protected double mutRate;
+	protected double killRate;
+	protected double popSize;
+	//curGraph is current best graph
+	protected ArrayList<Chromosome> population;
+	protected int fitConstant = -1;
 	public LocalSearchGA(ArrayList<Vertex> vertices) {
+		curGraph = vertices;
+		
 		System.out.println("Running the Local Search using a genetic algorithm");	
 	}
-
+	@Override
+	public ArrayList<Integer> runAlgo(){
+		curIterations = 0;
+		initialize();
+		while(curIterations < maxIterations){
+			Chromosome[] parents = selection();
+			Chromosome child = recombination(parents);
+			child = mutation(child);
+			child.fitness = evalFit(child);
+			replacement(child);
+			Chromosome best = getBest();
+			curGraph = best.genes;
+			conflictHistory.add(best.fitness);
+			curIterations++;
+		}
+		return conflictHistory;
+	}
 	@Override
 	protected int algorithm(int curV) {
 		// TODO Auto-generated method stub
@@ -24,6 +48,63 @@ public class LocalSearchGA extends AbstractAlgorithm {
 		// TODO Auto-generated method stub
 		
 	}
+	protected int evalFit(Chromosome c){
+		ArrayList<Integer[]> conflicts = findConflicts(c.genes);
+		return fitConstant*conflicts.size();
+	}
+	protected Chromosome getBest(){
+		return population.get(0);
+	}
+	protected void replacement(Chromosome growinUp){
+		
+	}
+	
+	protected void initialize(){
+		
+	}
+	protected Chromosome mutation(Chromosome frankie){
+		return frankie;
+	}
+	protected Chromosome[] selection(){
+		Chromosome[] parents = new Chromosome[2];
+		return parents;
+	}
+	protected Chromosome recombination(Chromosome[] parents){
+		return parents[0];
+	}
+	public double getMutRate() {
+		return mutRate;
+	}
+
+	public void setMutRate(double mutRate) {
+		this.mutRate = mutRate;
+	}
+
+	public double getKillRate() {
+		return killRate;
+	}
+
+	public void setKillRate(double killRate) {
+		this.killRate = killRate;
+	}
+
+	public double getPopSize() {
+		return popSize;
+	}
+
+	public void setPopSize(double popSize) {
+		this.popSize = popSize;
+	}
+
+	public ArrayList<Chromosome> getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(ArrayList<Chromosome> population) {
+		this.population = population;
+	}
+	
+	
 
 	
 
