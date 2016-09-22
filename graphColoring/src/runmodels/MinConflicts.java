d88d15775c91629d4797cd05ed2a321a7fdb7c81
Monica Thornton <monicasuecooper@hotmail.com>
2016-09-22 01:05:36 -0600

    Built parser in RunModels to pass vertices to the 5 different algorithms.

--- graphColoring/src/runmodels/MinConflicts.java
+++ graphColoring/src/runmodels/MinConflicts.java
@@ -6,32 +6,38 @@ public class MinConflicts extends AbstractAlgorithm {
 	
 	public MinConflicts(ArrayList<Vertex> vertices) {
 		System.out.println("Running the Min Conflicts algorithm");
-		
+//		
 		// makes sure the vertices were brought in correctly
+		System.out.println("Size: " + vertices.size());
+		
 		System.out.println("Vertices :");
 		for (int i = 0; i < vertices.size(); i++) {
-			System.out.println(vertices.get(i).getId());
+			//System.out.println(vertices.get(i).getId());
+			System.out.print(vertices.get(i).getId() + ", ");
 		}
+		
+		System.out.println();
 		
 		// and the edges
 		System.out.println("Edges: ");
 		for (Vertex v: vertices) {
 			for (int i = 0; i < v.neighbors.size(); i++) {
-				System.out.println(v.getId() + "," + v.getNeighbors().get(i).getId());
+				//System.out.println(v.getId() + "," + v.getNeighbors().get(i).getId());
+				System.out.print(v.getId() + "," + v.getNeighbors().get(i).getId() + "; ");
 			}
 		}
-		
-		//set colors
-		vertices.get(0).setColor(1);
-		vertices.get(1).setColor(2);
-		vertices.get(2).setColor(3);
-		vertices.get(3).setColor(4);
-		vertices.get(4).setColor(5);
-		
-		System.out.println("Color after setting");
-		for (Vertex v: vertices) {
-			System.out.println(v.getId() + "'s color: " + v.getColor());
-		}		
+//		
+//		//set colors
+//		vertices.get(0).setColor(1);
+//		vertices.get(1).setColor(2);
+//		vertices.get(2).setColor(3);
+//		vertices.get(3).setColor(4);
+//		vertices.get(4).setColor(5);
+//		
+//		System.out.println("Color after setting");
+//		for (Vertex v: vertices) {
+//			System.out.println(v.getId() + "'s color: " + v.getColor());
+//		}		
 		
 		
 		
