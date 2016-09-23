13c40d1d61687ef048c1688d1eed6e68c2ec7b25
Janette Marie Rounds <janette.rounds@msu.montana.edu>
2016-09-23 11:37:33 -0600

    Added comments for GA and Chromosome

--- graphColoring/src/runmodels/Chromosome.java
+++ graphColoring/src/runmodels/Chromosome.java
@@ -3,9 +3,9 @@ package runmodels;
 import java.util.ArrayList;
 
 public class Chromosome {
-	protected int numVertices;
-	public ArrayList<Vertex> genes = new ArrayList<Vertex>();
-	public int fitness;
+	protected int numVertices;//number of vertices
+	public ArrayList<Vertex> genes = new ArrayList<Vertex>();//the colored graph for this individual
+	public int fitness;//the number of conflicts in the colored graph
 	public Chromosome(ArrayList<Vertex> genes){
 		this.genes = genes;
 		numVertices = genes.size();
@@ -35,15 +35,15 @@ public class Chromosome {
 		//System.out.println(i + " " + color + " " +  genes.size());
 		genes.get(i).setColor(color);
 	}
-	public Chromosome clone(){
-		ArrayList<Vertex> geneCopy = new ArrayList<Vertex>();
-		for(int i = 0; i < numVertices; i++){
+	public Chromosome clone(){//copies the Chromosome
+		ArrayList<Vertex> geneCopy = new ArrayList<Vertex>();//Copy the graph
+		for(int i = 0; i < numVertices; i++){//add all vertexes to the graph
 			Vertex v = new Vertex(genes.get(i).getId(), genes.get(i).getXLoc(), genes.get(i).getYLoc(), genes.get(i).getColor());
 			v.createUsableColors(genes.get(i).usableColors.size());
 			geneCopy.add(v);
 		}
 		//System.out.println(geneCopy.size() + " " + numVertices);
-		for(int i = 0; i < numVertices;i++){
+		for(int i = 0; i < numVertices;i++){//add the neighbors to each vertex
 			ArrayList<Vertex> nei = genes.get(i).getNeighbors();
 			for(int j = 0; j < genes.get(i).neighbors.size(); j++){
 				
@@ -51,7 +51,7 @@ public class Chromosome {
 			}
 		}
 		//System.out.println(geneCopy.size() + " " + genes.size() + " " + numVertices);
-		Chromosome c = new Chromosome(geneCopy);
+		Chromosome c = new Chromosome(geneCopy);//copy the remaining parameters
 		c.fitness = this.fitness;
 		c.numVertices = this.numVertices;
 		return c;
